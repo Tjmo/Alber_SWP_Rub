17e18d835a4e671c56bd8fc90854dd30e1713d6f
Timo_Alber <tialber@office.htlinn.ac.at>
2022-12-13 07:00:07 +0100

    verbesserung

--- Stunde3/SPSES.py
+++ Stunde3/SPSES.py
@@ -6,52 +6,62 @@
 #         case _:
 #             return "Something's wrong with the internet"
 
+    
+
 
 
 import random
+from enum import IntEnum
 
-dic = {"stein" : 0, "papier" : 1, "schere" : 2, "echse" : 3, "spock" : 4}
+class Action(IntEnum):
+    Stein = 0
+    Papier = 1
+    Schere = 2
+    Echse = 3
+    Spock = 4
+
+victories = {
+    Action.Schere: [Action.Echse, Action.Papier],
+    Action.Papier: [Action.Spock, Action.Stein],
+    Action.Stein: [Action.Echse, Action.Schere],
+    Action.Echse: [Action.Spock, Action.Papier],
+    Action.Spock: [Action.Schere, Action.Stein]
+}
+
+
+def get_user_selection():
+    choices = [f"{action.name}[{action.value}]" for action in Action]
+    choices_str = ", ".join(choices)
+    selection = int(input(f"Enter a choice ({choices_str} (0-4)): "))
+    action = Action(selection)
+    return action
+
+def get_computer_selection():
+    selection = random.randint(0, len(Action) - 1)
+    action = Action(selection)
+    return action
+
+def determine_winner(user_action, computer_action):
+    defeats = victories[user_action]
+    if user_action == computer_action:
+        print(f"Both players selected {user_action.name}. It's a tie!")
+    elif computer_action in defeats:
+        print(f"{user_action.name} beats {computer_action.name}! You win!")
+    else:
+        print(f"{computer_action.name} beats {user_action.name}! You lose.")
 
 while True:
-
-    while True:
-        choice = input("Wähle (stein,papier,schere,echse,spock): ")
-        choice = choice.lower()
-
-        if choice not in dic.keys():
-            print("Falsche Eingabe!\n")
-
-        else:
-            pc = random.choice(list(dic.keys()))
-            pc = pc.lower()
-            break
-
-    if choice == pc:
-        print("{0:10}{1:10}".format("Spieler: ", choice[0].upper()+choice[1:]))
-        print("{0:10}{1:10}".format("Computer: ", pc[0].upper()+pc[1:]))
-        print("Unentschieden!")
-
-    elif (
-        (choice == "stein" and (pc == "schere" or pc == "echse")) or
-        (choice == "papier" and (pc == "stein" or pc == "spock")) or
-        (choice == "schere" and (pc == "papier" or pc == "echse")) or
-        (choice == "echse" and (pc == "papier" or pc == "spock")) or
-        (choice == "spock" and (pc == "stein" or pc == "schere"))
-        ):
-        print("{0:10}{1:10}".format("Spieler: ", choice[0].upper()+choice[1:]))
-        print("{0:10}{1:10}".format("Computer: ", pc[0].upper()+pc[1:]))
-        print("Du gewinnst!")
-
-    else:
-        print("{0:10}{1:10}".format("Spieler: ", choice[0].upper()+choice[1:]))
-        print("{0:10}{1:10}".format("Computer: ", pc[0].upper()+pc[1:]))
-        print("Der Computer gewinnt!")
-
-    again = input("\nNochmal (j/n): ")
-    if again == "J" or again == "j":
+    try:
+        user_action = get_user_selection()
+    except ValueError as e:
+        range_str = f"[0, {len(Action) - 1}]"
+        print(f"Invalid selection. Enter a value in range {range_str}")
         continue
-    else:
+
+    computer_action = get_computer_selection()
+    determine_winner(user_action, computer_action)
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
         break
 
-
-    

d6a4cb20a42db81283bb211e9e5db5b2190159e8
Timo_Alber <tialber@office.htlinn.ac.at>
2023-03-29 08:52:05 +0200

    WaehrendStunde

--- Stunde8/Proxy_Muster.java
+++ Stunde8/Proxy_Muster.java
@@ -1,11 +1,11 @@
 package Stunde8;
 
-// Eine Schnittstelle für einen Drucker, die die print-Methode definiert.
+// Eine Schnittstelle für an Drucker dia die print-Methode definiert
 interface Printer {
     void print(String text);
 }
 // a Druckerklasse dia die Printer-Schnittstelle implementiert
-// und en Text in Schwarzweiß druckt
+// und an Text in Schwarzweiß druckt
 class BlackAndWhitePrinter implements Printer {
     @Override
     public void print(String text) {
@@ -13,7 +13,7 @@ class BlackAndWhitePrinter implements Printer {
     }
 }
 // a Druckerklasse dia die Printer-Schnittstelle implementiert
-// und en Text in Farbe druckt
+// und an Text in Farbe druckt
 class ColorPrinter implements Printer {
     @Override
     public void print(String text) {
@@ -21,8 +21,8 @@ class ColorPrinter implements Printer {
     }
 }
 // a Proxy-Klasse für an Drucker, dia ebenfalls die Printer-Schnittstelle implementiert
-// die Klasse enthält eine Referenz auf einen konkreten Drucker und bietet a Methode
-// um den zugewiesenen Drucker während der Laufzeit zu ändern
+// die Klasse enthält a Referenz auf an konkreten Drucker und bietet a Methode
+// um en zugewiesenen Drucker während der Laufzeit zu ändern
 class PrinterProxy implements Printer {
     private Printer printer;
     
@@ -35,7 +35,7 @@ class PrinterProxy implements Printer {
         this.printer = newPrinter;
     }
     
-    // Implementierung der print-Methode der Printer-Schnittstelle,
+    // Implementierung der print-Methode der Printer-Schnittstelle
     // dia die print-Methode des zugewiesenen Druckers aufruaft
     @Override
     public void print(String text) {

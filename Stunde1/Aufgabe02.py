b91625c00ed98b59cfef1124712a64ab7d36789d
Timo_Alber <tialber@office.htlinn.ac.at>
2023-04-12 08:47:32 +0200

    prof_sieht_endlich_uploads_:)

--- Stunde1/Aufgabe02.py
+++ Stunde1/Aufgabe02.py
@@ -6,33 +6,31 @@ def get_random_six(startValue, endValue, rangeD):
         list_return.append(item)
     print(list_return)
 
-    for el in range(endValue):
+    for el in range(endValue):#------------
         lastPos = len(list_return)-el-1
-        ran = randint(startValue, endValue)
-        list_return[ran], list_return[-lastPos] = list_return[-lastPos], list_return[ran]
+        ran = randint(0, lastPos)
+        list_return[ran], list_return[-lastPos-1] = list_return[-lastPos-1], list_return[ran]
     print(list_return)
     return list_return[:-rangeD]
 
-def get_statistic(list):
-    for element in list:
-        list_start[element+startValueZ] = list_start[element+startValueZ] +1
+def get_statistic(lst):
+    for element in lst:
+        list_start[element-startValueZ] += 1
 
 
-def print_list(list):
-    counter = 0
-    for element in list:
-        print("Zahl", counter, ":", element)
-        counter = counter +1
+def print_list(lst):
+    for i, element in enumerate(lst):
+        print(f"Zahl {i}: {element}")
 
 endValueZ = 45
 startValueZ = 1
 rangeZ = 6
-list_start = [0] * (endValueZ-startValueZ)
+list_start = [0] * (endValueZ-startValueZ+1)
 
 for index in range(0, 1001):
     rand = get_random_six(startValueZ, endValueZ, rangeZ)
     print(rand)
     get_statistic(rand)
 
-
+print_list(list_start)
 
